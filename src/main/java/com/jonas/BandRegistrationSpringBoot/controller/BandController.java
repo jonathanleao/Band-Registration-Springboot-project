97db4f6bbc4fc0eda8891833bc1e58ddb48bf60c
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-06-06 13:29:59 -0300

    "Add pagination for controllers"

--- src/main/java/com/jonas/BandRegistrationSpringBoot/controller/BandController.java
+++ src/main/java/com/jonas/BandRegistrationSpringBoot/controller/BandController.java
@@ -7,6 +7,8 @@ import com.jonas.BandRegistrationSpringBoot.domain.Band;
 import com.jonas.BandRegistrationSpringBoot.services.BandServices;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,11 @@ import java.util.List;
 public class BandController {
 
     private final BandServices bandServices;
+
+    @GetMapping
+    public ResponseEntity<Page<Band>> findAll(Pageable pageable) {
+        return new ResponseEntity<>(bandServices.findAll(pageable), HttpStatus.OK);
+    }
 
     @GetMapping (path = "/{id}")
     public ResponseEntity<Band> findById(@PathVariable Integer id){
--- src/main/java/com/jonas/BandRegistrationSpringBoot/controller/LeaderController.java
+++ src/main/java/com/jonas/BandRegistrationSpringBoot/controller/LeaderController.java
@@ -7,6 +7,8 @@ import com.jonas.BandRegistrationSpringBoot.domain.Leader;
 import com.jonas.BandRegistrationSpringBoot.services.LeaderServices;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,33 +17,42 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping (path = "Leaders")
+@RequestMapping(path = "Leaders")
 @Log4j2
 public class LeaderController {
 
     private final LeaderServices leaderServices;
 
-    @GetMapping (path = "/{id}")
-    public ResponseEntity<Leader>findByID(@PathVariable Integer id){
-       return new ResponseEntity<>(leaderServices.findById(id), HttpStatus.OK);
+
+    @GetMapping
+    public ResponseEntity<Page<Leader>> findAll(Pageable pageable) {
+        return new ResponseEntity<>(leaderServices.findAll(pageable), HttpStatus.OK);
     }
-    @GetMapping (path = "/find")
-    public ResponseEntity<List<Leader>>findByName(@RequestParam String leaderName){
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Leader> findByID(@PathVariable Integer id) {
+        return new ResponseEntity<>(leaderServices.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/find")
+    public ResponseEntity<List<Leader>> findByName(@RequestParam String leaderName) {
         return new ResponseEntity<>(leaderServices.findByName(leaderName), HttpStatus.OK);
     }
+
     @PostMapping
-    public ResponseEntity<Leader> save(@RequestBody LeaderPostRequest leaderPostRequest){
+    public ResponseEntity<Leader> save(@RequestBody LeaderPostRequest leaderPostRequest) {
         return new ResponseEntity<>(leaderServices.save(leaderPostRequest), HttpStatus.CREATED);
     }
+
     @PutMapping
-    public ResponseEntity<Leader> update(@RequestBody LeaderPutRequest leaderPutRequest){
+    public ResponseEntity<Leader> update(@RequestBody LeaderPutRequest leaderPutRequest) {
         return new ResponseEntity<>(leaderServices.update(leaderPutRequest), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Void> delete (@PathVariable Integer id){
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         leaderServices.delete(id);
-        return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
